9670248ef1f2cc39d64da9693bb483e77c37400e
Matthew Chupp <mattlchupp@gmail.com>
2015-08-15 23:06:32 -0500

    added new jobs!

--- js/index.js
+++ js/index.js
@@ -7,6 +7,10 @@
 */
 		// variables for content 
 		var name = "Matt Chupp"; 
+		var freelanceJob = "Freelance Web Developer"
+		var freelanceYear = "Fall 2015 - Present";
+		var vineyardJob = "IT Support @ The Vineyard Church";
+		var vineyardYear = "Fall 2015 - Present";
 		var elevationJob = "IT Intern @ Elevation Church";
 		var elevationJobYear = "Summer 2015"; 
 		var bennerLibrary = "Web Developer @ Benner Library"; 
@@ -17,6 +21,26 @@
 		var Name = React.createClass({
 			render: function() {
     			return <div>{name}</div>;
+			}
+		});
+		var FreelanceJob = React.createClass({
+			render: function() {
+    			return <div>{freelanceJob}</div>;
+			}
+		});
+		var FreelanceYear = React.createClass({
+			render: function() {
+    			return <div>{freelanceYear}</div>;
+			}
+		});
+		var VineyardJob = React.createClass({
+			render: function() {
+    			return <div>{vineyardJob}</div>;
+			}
+		});
+		var VineyardYear = React.createClass({
+			render: function() {
+    			return <div>{vineyardYear}</div>;
 			}
 		});
 		var ElevationJob = React.createClass({
@@ -47,6 +71,10 @@
 
 		// render
 		React.render(<Name />, document.getElementById('name'));
+		React.render(<FreelanceJob />, document.getElementById('freelance'));
+		React.render(<FreelanceYear />, document.getElementById('freelance-year'));
+		React.render(<VineyardJob />, document.getElementById('vineyard'));
+		React.render(<VineyardYear />, document.getElementById('vineyard-year'));
 		React.render(<ElevationJob />, document.getElementById('elevation'));
 		React.render(<ElevationJobYear />, document.getElementById('elevation-year'));
 		React.render(<BennerLibrary />, document.getElementById('benner'));
